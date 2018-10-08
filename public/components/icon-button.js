e479829be062cc9ed0721cbfb9a87f12ad81dcbf
Daniel <danieliozzi@gmail.com>
2018-10-08 07:21:32 +0200

    css

--- public/components/icon-button.js
+++ public/components/icon-button.js
@@ -2,7 +2,13 @@ import { html } from "../node_modules/lit-html/lit-html.js";
 /**
  *
  */
-export default function IconButton({ onClick, icon, className }) {
-  const classes = `border-pill py-min max-content ${className}`;
+export default function IconButton({ onClick, icon, className, round }) {
+  const classes = [
+    round && "border-pill",
+    !round && "border-roundish",
+    `py-min max-content ${className}`,
+  ]
+    .filter(Boolean)
+    .join(" ");
   return html`<button class=${classes} @click=${onClick}>${icon}</button>`;
 }
